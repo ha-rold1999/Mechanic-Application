4b356f5730a9a0115c53fc525e56cf447aff1aa4
JayMar921 <jayharronabejar@gmail.com>
2023-03-03 22:37:21 +0800

    UI Update - ServiceRequest - Shop UI

--- Component/Home/MainComponent/Views/ShopViews/ShopView.js
+++ Component/Home/MainComponent/Views/ShopViews/ShopView.js
@@ -1,5 +1,12 @@
 import { useEffect, useState } from "react";
-import { View, Text, Button } from "react-native";
+import {
+  View,
+  Text,
+  Button,
+  Image,
+  StyleSheet,
+  ImageBackground,
+} from "react-native";
 import { useSelector } from "react-redux";
 import { useDispatch } from "react-redux";
 import { fetchService } from "../../../../../Redux/ProfileReducers/ServiceReducer";
@@ -20,35 +27,94 @@ export default function Shop(props) {
   const ServiceList = ({ ServiceName, Price, ServiceExpertise }) => {
     return (
       <>
-        <Text>{ServiceName}</Text>
-        <Text>{Price}</Text>
-        <Text>{ServiceExpertise}</Text>
+        <Text>Service: {ServiceName}</Text>
+        <Text>Price: {Price}</Text>
+        <Text>Expertise: {ServiceExpertise}</Text>
       </>
     );
   };
 
   return (
-    <View>
-      <Text>My Shop</Text>
-      <Text>{UUID}</Text>
-      <Text>{ShopName}</Text>
-      <Text>{ShopDescripction}</Text>
-      {serviceLst.length === 0 && <Text>You have no services offered</Text>}
-      {serviceLst.map(({ ServiceName, UUID, Price, ServiceExpertise }) => (
-        <View key={UUID}>
-          <ServiceList
-            ServiceName={ServiceName}
-            Price={Price}
-            ServiceExpertise={ServiceExpertise}
+    <View style={{ ...styles.bigContainer }}>
+      <ImageBackground
+        resizeMode={"contain"}
+        style={{
+          flex: 1,
+          position: "absolute",
+          width: "100%",
+          height: "100%",
+          opacity: 0.15,
+          backgroundColor: "#209589",
+        }}
+        source={require("../../../../../assets/Logo/ayus.png")}
+      ></ImageBackground>
+      <View style={{ height: "100%" }}>
+        <View style={{ ...styles.logoContainer }}>
+          <Text style={{ fontSize: 30 }}>Shop</Text>
+          <Image
+            style={{ ...styles.shopLogo }}
+            source={require("../../../../../assets/Icons/shop.png")}
           />
+
+          <Text style={{ fontSize: 20 }}>{ShopName}</Text>
+          <Text style={{ fontSize: 20 }}>{ShopDescripction}</Text>
         </View>
-      ))}
-      <Button
-        title="Add Service"
-        onPress={() =>
-          props.navigation.navigate("AddService", { mechanicID: UUID })
-        }
-      />
+
+        {serviceLst.length === 0 && <Text>You have no services offered</Text>}
+        <View style={{ width: "100%", alignItems: "center" }}>
+          {serviceLst.map(({ ServiceName, UUID, Price, ServiceExpertise }) => (
+            <View style={{ ...styles.serviceOffer }} key={UUID}>
+              <ServiceList
+                ServiceName={ServiceName}
+                Price={Price}
+                ServiceExpertise={ServiceExpertise}
+              />
+            </View>
+          ))}
+        </View>
+        <View style={{ ...styles.buttonContainer }}>
+          <View style={{ width: "50%" }}>
+            <Button
+              title="Add Service"
+              onPress={() =>
+                props.navigation.navigate("AddService", { mechanicID: UUID })
+              }
+            />
+          </View>
+        </View>
+      </View>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  shopLogo: {
+    width: 128,
+    height: 128,
+  },
+  logoContainer: {
+    position: "relative",
+    alignItems: "center",
+    textAlign: "center",
+  },
+  serviceOffer: {
+    width: "80%",
+    backgroundColor: "whitesmoke",
+    borderColor: "black",
+    borderWidth: 1,
+    margin: 10,
+    padding: 15,
+    borderRadius: 10,
+  },
+  buttonContainer: {
+    position: "absolute",
+    bottom: 10,
+    alignContent: "center",
+    alignItems: "center",
+    width: "100%",
+  },
+  bigContainer: {
+    height: "100%",
+    backgroundColor: "white",
+  },
+});
--- Component/Home/MapComponent/CustomerLocation.js
+++ Component/Home/MapComponent/CustomerLocation.js
@@ -52,6 +52,7 @@ const styles = StyleSheet.create({
   },
   map: {
     width: "100%",
-    height: "100%",
+    height: "400%",
+    maxHeight: 400,
   },
 });
