f2e8045680115581179bbeda61e3ac32ab46b1be
ha-rold1999 <htarimanc@gmail.com>
2023-04-10 13:17:13 +0800

    UI of rating modal improved

--- Component/Home/MainComponent/Views/MainViews/ReviewModal.js
+++ Component/Home/MainComponent/Views/MainViews/ReviewModal.js
@@ -1,4 +1,12 @@
-import { StyleSheet, Text, View, Modal, Button } from "react-native";
+import {
+  StyleSheet,
+  Text,
+  View,
+  Modal,
+  Button,
+  Pressable,
+  Image,
+} from "react-native";
 import { AirbnbRating } from "react-native-ratings";
 import { useDispatch, useSelector } from "react-redux";
 import React, { useState } from "react";
@@ -11,29 +19,119 @@ export default function ReviewModal(props) {
   return (
     <Modal
       animationType="slide"
-      transparent={true}
       visible={props.modalVisible}
+      transparent={true}
     >
-      <Text>{clienID}</Text>
-      <AirbnbRating
-        onFinishRating={(rating) => {
-          setRating(rating);
+      <View
+        style={{
+          flex: 1,
+          justifyContent: "center",
+          alignItems: "center",
         }}
-      />
-      <Button
-        title="Cancel"
-        onPress={() => {
-          props.setModalVisible(!props.modalVisible);
-        }}
-      />
-      <Button
-        title="Submit Rating"
-        onPress={() => {
-          dispatch(postReview(clienID, rating));
-          props.setModalVisible(!props.modalVisible);
-        }}
-      />
+      >
+        <View
+          style={{
+            backgroundColor: "white",
+            paddingHorizontal: 10,
+            paddingVertical: 10,
+            borderRadius: 20,
+            width: "90%",
+            elevation: 100,
+          }}
+        >
+          <View style={{ alignItems: "flex-end" }}>
+            <Pressable
+              onPress={() => {
+                props.setModalVisible(!props.modalVisible);
+              }}
+            >
+              <Image
+                source={require("../../../../../assets/Icons/cancel.png")}
+                style={{ width: 20, height: 20 }}
+              />
+            </Pressable>
+          </View>
+          <View style={{ alignItems: "center" }}>
+            <Text style={{ fontSize: 20, fontWeight: "700" }}>
+              Rate the service
+            </Text>
+          </View>
+          <View>
+            <AirbnbRating
+              onFinishRating={(rating) => {
+                setRating(rating);
+              }}
+            />
+            <View
+              style={{
+                flexDirection: "row",
+                justifyContent: "space-evenly",
+                paddingTop: 30,
+              }}
+            >
+              <Pressable
+                style={{
+                  paddingHorizontal: 20,
+                  backgroundColor: "#228BD4",
+                  paddingVertical: 10,
+                  elevation: 10,
+                  borderRadius: 10,
+                }}
+                onPress={() => {
+                  props.setModalVisible(!props.modalVisible);
+                }}
+              >
+                <Text style={{ color: "white", fontSize: 15 }}>Cancel</Text>
+              </Pressable>
+              <Pressable
+                style={{
+                  paddingHorizontal: 20,
+                  backgroundColor: "#209589",
+                  paddingVertical: 10,
+                  elevation: 10,
+                  borderRadius: 10,
+                }}
+                onPress={() => {
+                  dispatch(postReview(clienID, rating));
+                  props.setModalVisible(!props.modalVisible);
+                }}
+              >
+                <Text
+                  style={{ color: "white", fontSize: 15, fontWeight: "700" }}
+                >
+                  Submit
+                </Text>
+              </Pressable>
+            </View>
+          </View>
+        </View>
+      </View>
     </Modal>
+    // <Modal
+    //   animationType="slide"
+    //   transparent={true}
+    //   visible={props.modalVisible}
+    // >
+    //   <Text>{clienID}</Text>
+    //   <AirbnbRating
+    //     onFinishRating={(rating) => {
+    //       setRating(rating);
+    //     }}
+    //   />
+    //   <Button
+    //     title="Cancel"
+    //     onPress={() => {
+    //       props.setModalVisible(!props.modalVisible);
+    //     }}
+    //   />
+    //   <Button
+    //     title="Submit Rating"
+    //     onPress={() => {
+    //       dispatch(postReview(clienID, rating));
+    //       props.setModalVisible(!props.modalVisible);
+    //     }}
+    //   />
+    // </Modal>
   );
 }
 
--- Static.js
+++ Static.js
@@ -1,2 +1,2 @@
 export const apiKey = "API_SECRET-42e016b219421dc83d180bdee27f81dd";
-export const server = "http://192.168.254.104:5003";
+export const server = "http://192.168.1.38:5003";
