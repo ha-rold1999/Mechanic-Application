66004eb1e1b1b52e6af302c482dfa359ed5721c6
ha-rold1999 <htarimanc@gmail.com>
2023-04-11 04:10:30 +0800

    UI of logout improved

--- Component/Home/LogoutComponent/LogoutView.js
+++ Component/Home/LogoutComponent/LogoutView.js
@@ -1,17 +1,43 @@
-import { Button, StyleSheet, Text, View } from "react-native";
+import { Button, StyleSheet, Text, View, Pressable, Image } from "react-native";
 import React from "react";
 
 export default function LogoutView({ navigation }) {
   return (
-    <View>
-      <Text>Do you want to logout?</Text>
-      <Button
-        title="Yes"
+    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+      <Image
+        source={require("../../../assets/Icons/logout.png")}
+        style={{ width: 200, height: 200 }}
+      />
+      <Text style={{ fontSize: 20, fontWeight: "700" }}>
+        Do you want to logout
+      </Text>
+      <Pressable
+        style={{
+          backgroundColor: "#02599B",
+          paddingHorizontal: 100,
+          paddingVertical: 10,
+          borderRadius: 10,
+          marginTop: 10,
+        }}
         onPress={() => {
           navigation.reset({ index: 0, routes: [{ name: "Login" }] });
         }}
-      />
+      >
+        <Text style={{ color: "white", fontSize: 20, fontWeight: "700" }}>
+          Yes
+        </Text>
+      </Pressable>
     </View>
+
+    // <View>
+    //   <Text>Do you want to logout?</Text>
+    //   <Button
+    //     title="Yes"
+    //     onPress={() => {
+    //       navigation.reset({ index: 0, routes: [{ name: "Login" }] });
+    //     }}
+    //   />
+    // </View>
   );
 }
 
